d8c7027310b2bfb80cc3e4a99e58fdcd40d00894
Kazuki Nguyen <kazukiyoshida96@gmail.com>
2021-01-03 22:30:07 -0800

    :sparkles: Update the format of output

--- myTree/mytree.py
+++ myTree/mytree.py
@@ -3,13 +3,18 @@ import os
 from myTree import display
 
 
+SPACE = 4
+
+
 class TreeNode():
 
     def __init__(self, val=None):
         self.val = val
+        self.parent = None
         self.children = []
         self.child_num = 0
         self.depth = 0
+        self.is_lastoflist = False
 
     def buildTree(self, ignore_hidden=True):
         listdir = os.listdir(self.val)
@@ -17,18 +22,36 @@ class TreeNode():
             listdir = [x for x in listdir if x[0] != '.']
         listdir = [self.val + '/' + x for x in listdir]
 
-        for child in listdir:
+        for i in range(len(listdir)):
+            child = listdir[i]
             node = TreeNode(val=child)
             node.depth = self.depth + 1
             if os.path.isdir(node.val):
                 node.buildTree(ignore_hidden=ignore_hidden)
+            if i == len(listdir)-1:
+                node.is_lastoflist = True
+            node.parent = self
             self.children.append(node)
 
     def dfs(self):
+        prefix = ''
+        prefix = '│' + ' '*(SPACE-1)
+        prefix = prefix * (self.depth-2)
+        if self.depth > 0:
+            if self.depth > 1:
+                if self.parent is not None and self.parent.is_lastoflist:
+                    prefix = prefix + ' '*SPACE
+                else:
+                    prefix = prefix + '│' + ' '*(SPACE-1)
+            if self.is_lastoflist:
+                prefix = prefix + '└' + '─'*(SPACE-2) + ' '
+            else:
+                prefix = prefix + '├' + '─'*(SPACE-2) + ' '
+        print(prefix, end='')
         es = display.EscapeSequence()
         if os.path.isdir(self.val):
             es.setCharCyan()
-        print('    '*self.depth, self.val.split('/')[-1], sep='')
+        print(self.val.split('/')[-1])
         if os.path.isdir(self.val):
             es.resetChar()
         for child in self.children:
