02d1d476da0f7101d1f77c277df289bd752a0ecf
HN <kazukiyoshida96@gmail.com>
2021-03-03 19:56:10 -0800

    :pencil: Update coloring

--- myTree/__init__.py
+++ myTree/__init__.py
@@ -3,4 +3,4 @@ __version__ = '0.1.1'
 # 1~256
 # You can check the colors by 'python3 display.py'
 directory_color = 4
-color_suffixes = [('py', 14)]
+color_suffixes = [('py', 14), ('html', 166), ('css', 13)]
